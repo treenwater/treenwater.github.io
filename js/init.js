c7e8319bbb24a54c082487c03cae7998166e4f4e
Maker Doo <matsumaker@gmail.com>
2017-11-24 09:35:29 -0800

    firsty

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Aerial by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function() {
 
 	skel.init({
@@ -18,7 +12,7 @@
 	});
 
 	// Events (JS).
-		
+
 		// Remove "loading" class once the page has fully loaded.
 			window.onload = function() {
 				document.body.className = '';
